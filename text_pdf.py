46bd864ee191ff388eeea91f75812fcbf52f23fd
chrishowitt123 <62956122+chrishowitt123@users.noreply.github.com>
2020-06-14 00:37:56 +0100

    Update text_pdf.py

--- text_pdf.py
+++ text_pdf.py
@@ -1,6 +1,16 @@
-import pdfplumber
-pdf = pdfplumber.open('pdf-sample.pdf')
-page = pdf.pages[1]
-text = page.extract_text()
-print(text)
-pdf.close()
+import fitz
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk import Text
+
+pdf_document = "pdf-sample.pdf"
+doc = fitz.open(pdf_document)
+print ("number of pages: %i" % doc.pageCount)
+print(doc.metadata)
+
+page1 = doc.loadPage(0)
+page1text = page1.getText("text")
+
+tokens = word_tokenize(page1text)    
+textList = Text(tokens)
+page1text
