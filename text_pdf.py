c15ad176bd67766df18ba74be5dc8d2fdf327efa
chrishowitt123 <62956122+chrishowitt123@users.noreply.github.com>
2020-06-14 01:31:26 +0100

    Update text_pdf.py

--- text_pdf.py
+++ text_pdf.py
@@ -10,7 +10,7 @@ doc = fitz.open(pdf_document)
 print ("number of pages: %i" % doc.pageCount)
 print(doc.metadata)
 
-page1 = doc.loadPage(0)
+page1 = doc.loadPage(1)
 page1text = page1.getText("text")
 
     
@@ -20,4 +20,4 @@ tokens = sent_tokenize(page1text)
 tokens = [t.replace('\n', ', ') for t in tokens]
 tokens = str(tokens)
 tokens_list = tokens.split(",")
-tokens_list
+pd.DataFrame(tokens_list).to_csv('tok.csv')
