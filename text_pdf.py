e40879c107c99373a8074da96e99f81f14fbc1d8
chrishowitt123 <62956122+chrishowitt123@users.noreply.github.com>
2020-06-14 01:43:24 +0100

    Update text_pdf.py

--- text_pdf.py
+++ text_pdf.py
@@ -10,8 +10,8 @@ doc = fitz.open(pdf_document)
 print ("number of pages: %i" % doc.pageCount)
 print(doc.metadata)
 
-page1 = doc.loadPage(0)
-page1text = page1.getText("text")
+page = doc.loadPage(0)
+pagetext = page.getText("text")
 
     
 textList = Text(tokens)
