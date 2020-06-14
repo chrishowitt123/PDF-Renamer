134f8d00daa5ee7673ed8223a7cb4ded45d751a2
chrishowitt123 <62956122+chrishowitt123@users.noreply.github.com>
2020-06-14 16:35:12 +0100

    Update text_pdf.py

--- text_pdf.py
+++ text_pdf.py
@@ -1,43 +1,53 @@
 import fitz
+import glob 
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.tokenize import sent_tokenize
 from nltk import Text
 import shutil
+import datetime as dt
 
 
+path = r'C:\Users\chris\Desktop\pdf' #input('\nPaste the location of the files you wish to rename: ')
+path = path + '\*.pdf'
 
-path = r"Proformas_0406181628.pdf"
-doc = fitz.open(path)
-print ("number of pages: %i" % doc.pageCount)
-print(doc.metadata)
+files = []
 
-page = doc.loadPage(0)
-pagetext = page.getText("text")
+for file in glob.glob(path):
+    files.append(file)
+
+for file in files:
+    
+    doc = fitz.open(file)
+    print ("number of pages: %i" % doc.pageCount)
+    print(doc.metadata)
+
+    page = doc.loadPage(0)
+    pagetext = page.getText("text")
 
     
-textList = Text(tokens)
-tokens = sent_tokenize(pagetext)    
-tokens = [t.replace('\n', ', ') for t in tokens]
-tokens = str(tokens)
-tokens_list = tokens.split(",")
+    tokens = sent_tokenize(pagetext)    
+    textList = Text(tokens)
+    tokens = [t.replace('\n', ', ') for t in tokens]
+    tokens = str(tokens)
+    tokens_list = tokens.split(",")
 
-tokens_list = [t.strip() for t in tokens_list]
+    tokens_list = [t.strip() for t in tokens_list]
 
-tokens_list
+    tokens_list
 
-date_today = datetime.today().strftime('%Y-%m-%d')
-
+    date_today = dt.date.today().strftime('%Y-%m-%d')
+    
 #company_name_index = tokens_list.index('Holísticos – Serviços')
 #company_id_index = tokens_list.index('Código')
 
-company_name = tokens_list[82]
-company_id = tokens_list[22]
+    company_name = tokens_list[82]
+    company_id = tokens_list[22]
 
-new_filename = date_today + '_' + company_name + '_' + company_id + '.pdf'
+    new_filename = date_today + '_' + company_name + '_' + company_id + '.pdf'
 
 
-original = path
-target = new_filename
+    original = file
+    target = new_filename
 
-shutil.copyfile(original, target)
+    shutil.copyfile(original, target)
