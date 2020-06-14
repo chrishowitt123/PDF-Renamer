490dd85105a09f96944776963a88563c9b7cb3fb
chrishowitt123 <62956122+chrishowitt123@users.noreply.github.com>
2020-06-14 01:56:47 +0100

    Update text_pdf.py

--- text_pdf.py
+++ text_pdf.py
@@ -5,6 +5,7 @@ from nltk.tokenize import sent_tokenize
 from nltk import Text
 import pandas as pd
 
+
 pdf_document = "pdf-sample.pdf"
 doc = fitz.open(pdf_document)
 print ("number of pages: %i" % doc.pageCount)
@@ -20,6 +21,14 @@ tokens = sent_tokenize(page1text)
 tokens = [t.replace('\n', ', ') for t in tokens]
 tokens = str(tokens)
 tokens_list = tokens.split(",")
+
 items = pd.DataFrame(tokens_list)
+
 items.columns = ['Info']
-items.loc[6]
+
+Company_name = items.loc[6]
+Responsible = items.loc[19]
+
+Company_name = str(Company_name).replace('\nName:', '')
+Responsible = str(Responsible).replace('\nName:', '')
+Company_name + Responsible
