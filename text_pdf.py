14a39e863f27bf1a21860f877789dc208cdbd6ee
chrishowitt123 <62956122+chrishowitt123@users.noreply.github.com>
2020-06-24 22:04:38 +0100

    Update text_pdf.py

--- text_pdf.py
+++ text_pdf.py
@@ -1,59 +1,92 @@
 import fitz
 import glob 
-import nltk
 from nltk.tokenize import word_tokenize
 from nltk.tokenize import sent_tokenize
 from nltk import Text
 import shutil
 import datetime as dt
+import pandas as pd
+from os import listdir
+from os.path import isfile, join
+from pathlib import Path
+
 
 path = input('\nPaste the path of the PDFs you wish to rename: ') + '\\'
 path1 = path + '*.pdf'
 
 files = []
 
+
 for file in glob.glob(path1):
     files.append(file)
+  
+list_of_dict = []
+list_of_dict = [{} for i in range(0, len(files))]
 
+a = 0
+b = 0
+    
 for file in files:
     
-    doc = fitz.open(file)
+    doc = fitz.open(files[a])
     print ("\nnumber of pages: %i" % doc.pageCount)
     print(doc.metadata)
     print('\n')
-
+    print('\n')
+    
     page = doc.loadPage(0)
     pagetext = page.getText("text")
-
     
     tokens = sent_tokenize(pagetext)    
     textList = Text(tokens)
     tokens = [t.replace('\n', ', ') for t in tokens]
     tokens = str(tokens)
     tokens_list = tokens.split(",")
-
-    tokens_list = [t.strip() for t in tokens_list]
-
+    tokens_list = [t.strip() for t in tokens_list]    
+    
     for t, value in enumerate(tokens_list, 0):
-        print(t, value)
+        list_of_dict[b][t] = value
+        
+    for dic in list_of_dict:    
+        for k, v in dic.items():
+            print(k, ' : ', v)  
+
+    a += +1
+    b += +1 
+    
+
+        
 
 cn = input('\nType the number next to company name: ')
-cid = input('\nType the number next to company ID: ')
+cid = input('\nType the number next to company ID: ')    
 
 
-for file in files:
+company_name = []
+company_id = []
+date_today = [dt.date.today().strftime('%Y-%m-%d')]
+
+
+for dic in list_of_dict:
+    company_name.append(dic[int(cn)])
+    company_id.append(dic[int(cid)])
     
-    company_name = tokens_list[int(cn)]
-    company_id = tokens_list[int(cid)]
-    date_today = dt.date.today().strftime('%Y-%m-%d')
 
-    new_filename =   date_today +  '_' + company_name.title() + '_' + company_id + '.pdf'
+df = pd.DataFrame(list(zip(company_name, company_id, files)),
+              columns=['company_name','company_id', 'old_file_names'])
 
-    new_filename = path + new_filename
-        
-    print(file + '   --------->   ' + new_filename)
+df['date'] = str(pd.to_datetime('today').date())
 
-    original = file
-    target =  new_filename
+df.columns=['company_name','company_id', 'old_file_names', 'date']
 
-    shutil.copyfile(original, target)
+df['new_file_names'] = df['date'] + '_'  + df['company_name'] + '_' + df['company_id'] + '.pdf'
+
+old_file_names = list(df['old_file_names'])
+
+new_file_names = list(df['new_file_names'])
+
+
+for o, n in zip(old_file_names, new_file_names):
+    shutil.copyfile(o, n)  
+
+for o, n in zip(old, new):
+    print('\n ' + o + '    ---------->    ' + n)
