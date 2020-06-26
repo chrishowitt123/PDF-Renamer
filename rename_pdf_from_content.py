1f93ff672bfcb97c00cda172dd98072684f13b15
chrishowitt123 <62956122+chrishowitt123@users.noreply.github.com>
2020-06-26 13:01:15 +0100

    Update rename_pdf_from_content.py

--- rename_pdf_from_content.py
+++ rename_pdf_from_content.py
@@ -10,17 +10,18 @@ from os import listdir
 from os.path import isfile, join
 from pathlib import Path
 
+
 path = input('\nPaste the path of the PDFs you wish to rename: ') + '\\'
-path1 = path + '*.pdf' # Create glob path
+path1 = path + '*.pdf'
 
 files = []
 
-for file in glob.glob(path1):
-    files.append(file) # Create list of PDF paths
-  
 
+for file in glob.glob(path1):
+    files.append(file)
+  
 list_of_dict = []
-list_of_dict = [{} for i in range(0, len(files))] # Create a list of dicts as per the length of the PDF files list
+list_of_dict = [{} for i in range(0, len(files))]
 
 a = 0
 b = 0
@@ -29,43 +30,46 @@ for file in files:
     
     doc = fitz.open(files[a])
     print ("\nnumber of pages: %i" % doc.pageCount)
-    print(doc.metadata) # Print metadata for each PDF
-    print('\n')
+    print(doc.metadata)
     print('\n')
     
     page = doc.loadPage(0)
-    pagetext = page.getText("text") # Import text from chosen page that has the rename strings 
+    pagetext = page.getText("text")
     
-    tokens = sent_tokenize(pagetext)  # Tokenize text as list  
+    tokens = sent_tokenize(pagetext)    
     textList = Text(tokens)
     tokens = [t.replace('\n', ', ') for t in tokens]
     tokens = str(tokens)
     tokens_list = tokens.split(",")
     tokens_list = [t.strip() for t in tokens_list]    
     
-    for t, value in enumerate(tokens_list, 0): # Enumrrate each token in dicts (text from PDFs)
-        list_of_dict[b][t] = value # Assign tokens as dict values
-        
-    for dic in list_of_dict:    
-        for k, v in dic.items():
-            print(k, ' : ', v)   # Print text in easy to read format for user to confirm when the strings for renaming are
+    for t, value in enumerate(tokens_list, 0):
+        list_of_dict[b][t] = value 
 
     a += +1
     b += +1 
+    
+for dic in list_of_dict:    
+    for k, v in dic.items():
+        print(k, ' : ', v)
+        
     
 
 cn = input('\nType the number next to company name: ')
 cid = input('\nType the number next to company ID: ')    
 
+
 company_name = []
 company_id = []
 date_today = [dt.date.today().strftime('%Y-%m-%d')]
 
-for dic in list_of_dict:
-    company_name.append(dic[int(cn)]) # Create seperate lists for each part of new file name
-    company_id.append(dic[int(cid)])
 
-df = pd.DataFrame(list(zip(company_name, company_id, files)), # Create new file names on Pandas
+for dic in list_of_dict:
+    company_name.append(dic[int(cn)])
+    company_id.append(dic[int(cid)])
+    
+
+df = pd.DataFrame(list(zip(company_name, company_id, files)),
               columns=['company_name','company_id', 'old_file_names'])
 
 df['date'] = str(pd.to_datetime('today').date())
@@ -78,8 +82,9 @@ old_file_names = list(df['old_file_names'])
 
 new_file_names = list(df['new_file_names'])
 
-for o, n in zip(old_file_names, new_file_names): # Copy old file as to still have the orignals after prjram termination
+
+for o, n in zip(old_file_names, new_file_names):
     shutil.copyfile(o, n)  
 
-for o, n in zip(old, new):
-    print('\n ' + o + '    ---------->    ' + n) # Tell user which files have been changed to what
+for o, n in zip(old_file_names, new_file_names):
+    print('\n ' + o + '    ---------->    ' + n)
