230964c8a0d3076955d7f5dcb23be80180118e43
Chris Howitt <62956122+chrishowitt123@users.noreply.github.com>
2022-09-17 18:41:35 +0100

    Update PDF-Renamer.py

--- PDF-Renamer.py
+++ PDF-Renamer.py
@@ -10,80 +10,87 @@ from os import listdir
 from os.path import isfile, join
 from pathlib import Path
 
-path = input('\nPaste the path of the PDFs you wish to rename: ') + '\\' # Paste the path to your files
-path1 = path + '*.pdf' # create glob path that recognizes all PDFs
+# paste path to files
+path = input('\nPaste the path of the PDFs you wish to rename: ') + '\\'
+
+# create glob path that recognizes all PDFs
+path1 = path + '*.pdf' 
 
 files = []
 
-
+# append file names to files list
 for file in glob.glob(path1):
-    files.append(file) # Create list of PDFs to rename
-  
+    files.append(file)
+    
+# create list of dictionaries relative to the number of files to rename
 list_of_dict = []
-list_of_dict = [{} for i in range(0, len(files))] # Create list of dicts as per the number of files to rename
+list_of_dict = [{} for i in range(0, len(files))]
 
 a = 0
 b = 0
     
 for file in files:
-    
-    doc = fitz.open(files[a]) # Display metadata of PDFs
+    doc = fitz.open(files[a])
+    # display metadata of PDFs
     print ("\nnumber of pages: %i" % doc.pageCount)
     print(doc.metadata)
     print('\n')
     
-    page = doc.loadPage(0) # Choose where the rename content is (usally first or second page)
+    # choose where the rename content is (usually first or second page)
+    page = doc.loadPage(0) 
     pagetext = page.getText("text")
     
-    tokens = sent_tokenize(pagetext) # Create tokenized groups of words seperated by \n in the PDFs    
+    # create tokenized word groups by PDF
+    tokens = sent_tokenize(pagetext)  
     textList = Text(tokens)
     tokens = [t.replace('\n', ', ') for t in tokens]
     tokens = str(tokens)
     tokens_list = tokens.split(",")
     tokens_list = [t.strip() for t in tokens_list]    
     
-    for t, value in enumerate(tokens_list, 0): # Enumerate word groups and add to the dicts
+    # enumerate word groups and add to list_of_dict
+    for t, value in enumerate(tokens_list, 0): 
         list_of_dict[b][t] = value 
 
     a += +1
     b += +1 
     
-for dic in list_of_dict: # Display the enumerated groups of words to the user for file name selection  
+# display the enumerated word groups to the user for file name selection      
+for dic in list_of_dict: 
     for k, v in dic.items():
         print(k, ' : ', v)
         
     
-
-cn = input('\nType the number next to company name: ') # Select file name sections by number
+# select file name sections by number
+cn = input('\nType the number next to company name: ') 
 cid = input('\nType the number next to company ID: ')    
-
 
 company_name = []
 company_id = []
 date_today = [dt.date.today().strftime('%Y-%m-%d')]
 
-
-for dic in list_of_dict: # Store file rename vales in lists
+# store file and rename vales in lists
+for dic in list_of_dict: 
     company_name.append(dic[int(cn)])
     company_id.append(dic[int(cid)])
     
 
-df = pd.DataFrame(list(zip(company_name, company_id, files)), # Create new file names
+# create new file names
+df = pd.DataFrame(list(zip(company_name, company_id, files)), 
               columns=['company_name','company_id', 'old_file_names'])
 
 df['date'] = str(pd.to_datetime('today').date())
-
 df.columns=['company_name','company_id', 'old_file_names', 'date']
-
 df['new_file_names'] = df['date'] + '_'  + df['company_name'] + '_' + df['company_id'] + '.pdf'
 
 old_file_names = list(df['old_file_names'])
-
 new_file_names = list(df['new_file_names'])
 
 
-for o, n in zip(old_file_names, new_file_names): # Copy orignal files and rename copies as per user definitions
+# copy original files and rename copies as per user definitions
+for o, n in zip(old_file_names, new_file_names): 
     shutil.copyfile(o, n)  
 
-for o, n in zip(old_file_names, new_file_names): # Print what has been renamed to what
+# print what has been renamed to what
+for o, n in zip(old_file_names, new_file_names): 
     print('\n ' + o + '    ---------->    ' + n)
